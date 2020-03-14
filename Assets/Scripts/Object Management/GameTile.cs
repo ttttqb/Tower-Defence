7e2570c9cfe7e76d92bf7546da6e154bcb0f1c3b
ttttqb <tanqinbing@hotmail.com>
2020-03-14 18:34:29 +0800

    The board 2.2

--- Assets/Scripts/Object Management/GameTile.cs
+++ Assets/Scripts/Object Management/GameTile.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GameTile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameTile : MonoBehaviour {
+	
+	[SerializeField]
+	Transform arrow = default;
+
+    GameTile north, east, south, west, nextOnPath;
+
+    int distance;
+
+    public bool HasPath => distance != int.MaxValue;
+
+	public static void MakeEastWestNeighbors (GameTile east, GameTile west) {
+		Debug.Assert(
+			west.east == null && east.west == null, "Redefined neighbors!"
+		);
+		west.east = east;
+		east.west = west;
+	}
+
+	public static void MakeNorthSouthNeighbors (GameTile north, GameTile south) {
+		Debug.Assert(
+			south.north == null && north.south == null, "Redefined neighbors!"
+		);
+		south.north = north;
+		north.south = south;
+	}
+
+    public void ClearPath(){
+        distance = int.MaxValue;
+        nextOnPath = null;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void BecomeDestination() {
+        distance = 0;
+        nextOnPath = null;
     }
 }
