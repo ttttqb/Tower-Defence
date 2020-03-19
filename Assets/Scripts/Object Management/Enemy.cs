2b4d3e672c457d56641a89c41ec6f330959321bd
ttttqb <tanqinbing@hotmail.com>
2020-03-19 12:58:32 +0800

    Finish 3.5

--- Assets/Scripts/Object Management/Enemy.cs
+++ Assets/Scripts/Object Management/Enemy.cs
@@ -21,16 +21,31 @@ namespace Object_Management
 		private Vector3 positionFrom, positionTo;
 		private float progress;
 
-		public void SpawnOn (GameTile tile)
+		Direction direction;
+		DirectionChange directionChange;
+		float directionAngleFrom, directionAngleTo;
+
+		public void SpawnOn(GameTile tile)
 		{
 			//transform.localPosition = tile.transform.localPosition;
 			Debug.Assert(tile.NextTileOnPath != null, "Nowhere to go!", this);
 			tileFrom = tile;
 			tileTo = tile.NextTileOnPath;
+			// positionFrom = tileFrom.transform.localPosition;
+			// positionTo = tileFrom.ExitPoint;
+			// transform.localRotation = tileFrom.PathDirection.GetRotation();
+			progress = 0f;
+			PrepareIntro();
+		}
+
+		void PrepareIntro()
+		{
 			positionFrom = tileFrom.transform.localPosition;
 			positionTo = tileFrom.ExitPoint;
-			transform.localRotation = tileFrom.PathDirection.GetRotation();
-			progress = 0f;
+			direction = tileFrom.PathDirection;
+			directionChange = DirectionChange.None;
+			directionAngleFrom = directionAngleTo = direction.GetAngle();
+			transform.localRotation = direction.GetRotation();
 		}
 
 		public bool GameUpdate()
@@ -45,14 +60,69 @@ namespace Object_Management
 					OriginFactory.Reclaim(this);
 					return false;
 				}
-				positionFrom = positionTo;
-				positionTo = tileFrom.ExitPoint;
-				transform.localRotation = tileFrom.PathDirection.GetRotation();
+
+				// positionFrom = positionTo;
+				// positionTo = tileFrom.ExitPoint;
+				// transform.localRotation = tileFrom.PathDirection.GetRotation();
 				progress -= 1f;
+				PrepareNextState();
 			}
 
 			transform.localPosition = Vector3.LerpUnclamped(positionFrom, positionTo, progress);
+			if (directionChange != DirectionChange.None)
+			{
+				float angle = Mathf.LerpUnclamped(
+					directionAngleFrom, directionAngleTo, progress
+				);
+				transform.localRotation = Quaternion.Euler(0f, angle, 0f);
+			}
+
 			return true;
+		}
+
+		void PrepareNextState()
+		{
+			positionFrom = positionTo;
+			positionTo = tileFrom.ExitPoint;
+			directionChange = direction.GetDirectionChangeTo(tileFrom.PathDirection);
+			direction = tileFrom.PathDirection;
+			directionAngleFrom = directionAngleTo;
+			switch (directionChange)
+			{
+				case DirectionChange.None:
+					PrepareForward();
+					break;
+				case DirectionChange.TurnRight:
+					PrepareTurnRight();
+					break;
+				case DirectionChange.TurnLeft:
+					PrepareTurnLeft();
+					break;
+				default:
+					PrepareTurnAround();
+					break;
+			}
+		}
+
+		void PrepareForward()
+		{
+			transform.localRotation = direction.GetRotation();
+			directionAngleTo = direction.GetAngle();
+		}
+
+		void PrepareTurnRight()
+		{
+			directionAngleTo = directionAngleFrom + 90f;
+		}
+
+		void PrepareTurnLeft()
+		{
+			directionAngleTo = directionAngleFrom - 90f;
+		}
+
+		void PrepareTurnAround()
+		{
+			directionAngleTo = directionAngleFrom + 180f;
 		}
 	}
 }
