1549276a11881d1173d4c3ea6fc029e885445598
ttttqb <tanqinbing@hotmail.com>
2020-03-24 19:31:46 +0800

    Finish Animation 4

---
+++ Assets/Scripts/EnemyAnimationConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyAnimationConfig : ScriptableObject
+{
+    [SerializeField] private AnimationClip move = default,
+        intro = default, 
+        outro = default;
+    
+    public AnimationClip Move => move;
+    public AnimationClip Intro => intro;
+    public AnimationClip Outro => outro;
+
+}
